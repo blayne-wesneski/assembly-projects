1dc58044fef524ed55950ead6e29b90351321bbd
CaveStreamGames <102747184+blayne-wesneski@users.noreply.github.com>
2022-09-20 09:48:45 -0400

    Create Project 2.asm
    Created file with basic code structure to build on top of

---
+++ Project2.asm
@@ -0,0 +1,16 @@
+;Author: Blayne Wesneski
+;Description: Second Project (Shift Cipher Encryption)
+;Date: September 20th 2022
+
+
+;.data holds the necessary known Variables/Constants
+;This is stored in the program itself
+section .data
+
+
+;Undefined variables go in .bss
+section .bss
+
+
+section .text
+global _start
