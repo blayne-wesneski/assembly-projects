3761563adaf71602b98161103ca8294203ed6169
CaveStreamGames <102747184+blayne-wesneski@users.noreply.github.com>
2022-10-13 20:37:49 -0400

    Update Project2.asm

--- Project2.asm
+++ Project2.asm
@@ -7,10 +7,31 @@
 ;This is stored in the program itself
 section .data
 
+    Input: times 64 db 0 ;64 byte buffer
+
+    Prompt:                 db  "Please type in a word: ", 0x00
+    PromptLen:              EQU $-Prompt
+
+    Response:               db  "You typed: ", 0x00
+    ResponseLen:            EQU $-Response
 
 ;Undefined variables go in .bss
 section .bss
 
 
 section .text
+
 global _start
+
+_start:
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, Prompt
+    mov rdx, PromptLen
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, Input
+    mov rdx, 64
