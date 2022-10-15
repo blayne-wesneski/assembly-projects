6edd59975e75840735022f7a4f952f172af316e7
CaveStreamGames <102747184+blayne-wesneski@users.noreply.github.com>
2022-10-14 20:27:23 -0400

    Update Project2.asm
    this is probably horribly wrong

--- Project2.asm
+++ Project2.asm
@@ -7,7 +7,7 @@
 ;This is stored in the program itself
 section .data
 
-    Input: times 64 db 0 ;64 byte buffer
+    Input: times 64 db 0 ;64 byte input buffer
 
     Prompt:                 db  "Please type in a word: ", 0x00
     PromptLen:              EQU $-Prompt
@@ -42,6 +42,57 @@ _start:
     mov rsi, Input
     mov rdx, 64
     syscall
+
+_rotate:
+
+    mov rdi, Input
+    mov rax, 26
+    mov esi, 3 ;key
+
+_loop:
+
+    ;load character from Input
+    mov ch, [rdi]
+
+    mov cl, ch
+    or cl, 0x20
+    cmp cl, 'a'
+    jl ,no_encoding
+    cmp cl, 'z'
+    jl .no_encoding
+
+    and cl, 0x1f
+    add cl, esi
+
+    dec cl
+    xor ax, ax
+    mov al, cl
+    div rax
+    inc ah
+
+    and ch, 0xE0
+    or ch, ah
+
+.no_encoding:
+ 
+    ;; write to the result
+    mov [rdx], ch
+
+    ;; prepare next iteration
+    inc rdi
+    inc rdx
+
+    ;; more to do?
+    and ch, ch
+    jnz _loop
+    ret
+
+output:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rdx
+    mov rdx, 64
+    exit
 
 exit:
     mov rax, 60
