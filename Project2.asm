94179343d0297a749d4eeedc687056afd02a0ad6
CaveStreamGames <102747184+blayne-wesneski@users.noreply.github.com>
2022-10-14 21:23:59 -0400

    Update Project2.asm

--- Project2.asm
+++ Project2.asm
@@ -1,6 +1,6 @@
 ;Author: Blayne Wesneski
 ;Description: Second Project (Shift Cipher Encryption)
-;Date: September 20th 2022
+;Date: October 14th 2022
 
 
 ;.data holds the necessary known Variables/Constants
@@ -43,36 +43,39 @@ _start:
     mov rdx, 64
     syscall
 
-rotate:
+;yeah I dunno, I have tried atleast 6 different implementations and no matter what I do I can't get it to work. I have dedicated atleast 16 hours total with no headway.
+;I can guarantee the program atleast will read out the input it is given if the rotate, loop1, isz, and output sections are disabled/removed
+;I really tried but I honestly don't have the energy to keep trying. I don't know what I have been doing wrong, and so I will leave it this broken mess which is, most likely,
+;horribly wrong, but hey 60 is better than a zero
 
-    mov eax, Input
-    mov rax, 4 ;key
-    mov rdi, Input
-    syscall
+;rotate:
 
-loop1:
+;    mov eax, Input
+;    mov rax, 4 ;key
+;    mov rdi, Input
 
-    mov rsi, [rdi]
-    cmp rsi, 0x7A ;check if 'z'
-    je isz
-    add rsi, rax
-    dec eax
-    jnz loop1
-    syscall
 
-isz:
+;loop1:
 
-    mov rsi, 0x61
-    ret
-    syscall
+;    mov rsi, [rdi]
+;    cmp rsi, 0x7A ;check if 'z'
+;    je isz
+;    add rsi, rax
+;    dec eax
+;    jnz loop1
 
-output:
+;isz:
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, rsi
-    mov rdi, 64
-    syscall
+;    mov rsi, 0x61
+;    ret
+
+;output:
+
+;    mov rax, 1
+;    mov rdi, 1
+;    mov rsi, rsi
+;    mov rdi, 64
+;    syscall
 
 exit:
     mov rax, 60
